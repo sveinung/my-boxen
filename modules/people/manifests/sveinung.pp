2a44333793a0176477e12176cf2c92fd007a0e9f
Sveinung Dalatun <sdalatun@gmail.com>
2013-10-06 13:00:11 +0200

    .gitconfig

--- modules/people/manifests/sveinung.pp
+++ modules/people/manifests/sveinung.pp
@@ -1,4 +1,17 @@
 class people::sveinung {
+  $home     = "/Users/${::boxen_user}"
+  $my       = "${home}/my"
+  $dotfiles = "${my}/dotfiles"
+
+  file { $my:
+    ensure  => directory
+  }
+
+  repository { $dotfiles:
+    source  => 'sveinung/dotfiles',
+    require => File[$my]
+  }
+
   include spotify
   include macvim
   include iterm2::stable
@@ -6,5 +19,10 @@ class people::sveinung {
   class { 'intellij':
     edition => 'community',
     version => '12.1.5',
+  }
+
+  file { "${home}/.gitconfig":
+    ensure => present,
+    source => "${dotfiles}/gitconfig",
   }
 }
