de430c6ee89251bc8029d720cac3018cbee506c1
Sveinung Dalatun <sdalatun@gmail.com>
2013-10-06 15:20:12 +0200

    .vimrc

--- modules/people/manifests/sveinung.pp
+++ modules/people/manifests/sveinung.pp
@@ -13,7 +13,6 @@ class people::sveinung {
   }
 
   include spotify
-  include macvim
   include iterm2::stable
   include sbt
   include caffeine
@@ -28,8 +27,17 @@ class people::sveinung {
   file { "${home}/.gitconfig":
     ensure => present,
     source => "${dotfiles}/gitconfig",
+    require => Repository[$dotfiles],
   }
 
   class { 'nodejs::global': version => 'v0.10.5' }
   nodejs::module { 'bower': node_version => 'v0.10' }
+
+  include macvim
+
+  file { "${home}/.vimrc":
+    ensure => present,
+    source => "${dotfiles}/vimrc",
+    require => Repository[$dotfiles],
+  }
 }
